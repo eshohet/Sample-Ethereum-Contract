4eb960a1ceaf6cc078cbb787418085d553e3449f
Elliot Shohet <elliot.shohet@gmail.com>
2017-08-31 19:46:12 -0700

    Deploy contracts synchronously

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,13 +3,9 @@ var SimpleCompany = artifacts.require("./SimpleCompany.sol");
 
 module.exports = function(deployer, network, accounts) {
 
-    deployer.deploy(Faucet);
-
-    deployer.deploy(SimpleCompany, Faucet.address, + new Date() + 300000).then(function() {
-        web3.eth.sendTransaction({from: accounts[0], to: SimpleCompany.address, value: 100000000000000000000}, function(err, res) {
-            if(err)
-                console.log(err)
-            return;
+    deployer.deploy(Faucet).then(function() {
+        return deployer.deploy(SimpleCompany, Faucet.address, + new Date() + 300000).then(function() {
+            return web3.eth.sendTransaction({from: accounts[0], to: SimpleCompany.address, value: 100000000000000000000});
         });
     });
 };

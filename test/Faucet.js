d5c8b5c3808262fe9d0219156f109f4e67d6636d
Elliot Shohet <elliot.shohet@gmail.com>
2017-09-02 12:51:36 -0700

    Check if tokens dispense correctly

--- test/Faucet.js
+++ test/Faucet.js
@@ -2,10 +2,19 @@ var Faucet = artifacts.require("./Faucet.sol");
 // ... more code
 contract('Faucet', function(accounts) {
 
-    it("check if faucet symbol is BET", async function() {
+    it("faucet deployed with BET symbol", async function() {
         let faucet = await Faucet.deployed();
         let symbol = await faucet.symbol.call();
         assert.equal(symbol, "BET", "Symbol name is not BET");
     });
+
+    it("dispense tokens", async function() {
+        let faucet = await Faucet.deployed();
+        let FAUCET_AMOUNT = await faucet.FAUCET_AMOUNT.call();
+        await faucet.dispense({from: accounts[0]});
+        let accountBalance = await faucet.balanceOf.call(accounts[0]);
+        assert.equal(FAUCET_AMOUNT.valueOf(), accountBalance.valueOf(), "account balance not equal to dispensed amount");
+    });
+
 
 });
